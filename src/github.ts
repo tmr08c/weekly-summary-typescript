16f23882948d11d9ac569f85a822cbd2e18de734
tmr08c <tmr08c@gmail.com>
2019-05-13 01:39:16 -0400

    Supports dynamic date range for fetching closed pull requests.
    Currently, while dynamic, the date range will always be the last week.
    Eventually, we could add some customization around this (e.g., passing
    in start or end date, etc.).
    
    A big part of this was turning the GitHub module into a class, to make
    mocking and dependency injection easier. By changing it to a class, and
    having `index.ts` take it as an argument with a default, we were able to
    write tests that pass in a fake version (see `FakeService` in
    `idnex.test.ts`). The fake version could have the API call mocked out,
    making testing the date stuff easier.
    
    Moving away from a hard coded date to a dyanmic date range, I learned
    the syntax for a date range in the GitHub search is `date1..date2` (as
    opposed to something like `closed:>date1 closed:<date2`).
    
    This commit also contains a small tweak to `tsconfig`.
    
    -    // "lib": [],                             /* Specify library files to be included in the compilation. */
    +    "lib": [
    +      "es2015"
    
    To be honest, not 100% why this was necessary, but it was the
    recommendation for compiler error.

--- src/github.ts
+++ src/github.ts
@@ -1,21 +1,6 @@
-/// <reference path="index.d.ts" />
-
 const graphql = require("@octokit/graphql");
 
-interface SearchResponse {
-  search: {
-    edges: [{ node: Response }];
-  };
-}
-
-interface Response {
-  repository: { name: string };
-  title: string;
-  createdAt: Date;
-  closedAt: Date;
-  url: string;
-  merged: boolean;
-}
+import { format } from "date-fns";
 
 const closedPullRequestsQuery = `
   query recentPullRequests($searchString: String!) {
@@ -41,22 +26,51 @@ const closedPullRequestsQuery = `
     }
  }
 `;
-
-export async function recentlyClosedPullRequests({
-  organization
-}: WeeklySummary.RequestParams): Promise<SearchResponse> {
-  return await graphql({
-    query: closedPullRequestsQuery,
-    searchString: searchQueryString({ organization }),
-    headers: {
-      authorization: `token ${process.env.GITHUB_AUTH_TOKEN}`
-    }
-  }).catch((e: Error) => {
-    console.error(`Failed to make request to GitHub. Received: ${e.message}`);
-    throw e;
-  });
+interface RequestParams {
+  organization: string;
+  startDate: Date;
+  endDate: Date;
 }
 
-function searchQueryString({ organization }: { organization: string }) {
-  return `org:${organization} is:pr is:closed is:merged closed:>2019-04-01`;
+interface SearchResponse {
+  search: {
+    edges: [{ node: Response }];
+  };
+}
+
+interface Response {
+  repository: { name: string };
+  title: string;
+  createdAt: Date;
+  closedAt: Date;
+  url: string;
+  merged: boolean;
+}
+
+export class GitHub {
+  static async recentlyClosedPullRequests(
+    params: RequestParams
+  ): Promise<SearchResponse> {
+    return await graphql({
+      query: closedPullRequestsQuery,
+      searchString: this.searchQueryString(params),
+      headers: {
+        authorization: `token ${process.env.GITHUB_AUTH_TOKEN}`
+      }
+    }).catch((e: Error) => {
+      console.error(`Failed to make request to GitHub. Received: ${e.message}`);
+      throw e;
+    });
+  }
+
+  static searchQueryString({
+    organization,
+    startDate,
+    endDate
+  }: RequestParams) {
+    const formattedStartDate = format(startDate, "YYYY-MM-DD");
+    const formattedEndDate = format(endDate, "YYYY-MM-DD");
+
+    return `org:${organization} is:pr is:closed is:merged closed:${formattedStartDate}..${formattedEndDate}`;
+  }
 }
--- src/index.test.ts
+++ src/index.test.ts
@@ -1,8 +1,24 @@
 import { fetchRecentlyClosedPullRequests } from "./index";
+import MockDate from "mockdate";
 
-test("gives recently closed issues for public repositories", () => {
-  // const recentlyClosedPullRequests = fetchRecentlyClosedPullRequests({
-  //   organization: "microsoft",
-  // });
-  // expect(recentlyClosedPullRequests.length).toBeGreaterThan(1);
+// Reset Date back to the real date
+afterEach(() => MockDate.reset());
+
+class FakeService {
+  static async recentlyClosedPullRequests(): Promise<any> {}
+  static searchQueryString(): any {}
+}
+
+const fakeApiCall = (FakeService.recentlyClosedPullRequests = jest.fn());
+
+test("dates default to the last week", () => {
+  MockDate.set("1/8/2019");
+
+  fetchRecentlyClosedPullRequests({ organization: "my-org" }, FakeService);
+
+  expect(fakeApiCall).toHaveBeenCalledWith({
+    organization: "my-org",
+    startDate: new Date("1/1/2019"),
+    endDate: new Date("1/7/2019")
+  });
 });
